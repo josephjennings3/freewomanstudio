04c025a68c6ad3d1604969fa8a9c68325a592276
Joseph <jajenningsiii@gmail.com>
2020-09-13 02:14:55 -0700

    created footer, footer css, saved about img, beginning about page

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../css/footer.css';
+import { SocialIcon } from 'react-social-icons';
 
-function Footer() {
-  return (
-    <div className="footer">
-      <main>
-        <h1>Footer</h1>
-      </main>
-    </div>
-  );
+class Footer extends Component {
+  render() {
+    return (
+      <div className="footer">
+          <div className="footer-container">
+              <section className="footer-contact">
+                  <div className="footer-icon">
+                      <a href="mailto:jajenningsiii@email.com" rel="noopener noreferrer"><SocialIcon network="email" color="#fff" style={{ height: 30, width: 30 }}/></a>
+                  </div>
+                  <div className="footer-icon">
+                      <a href="https://www.instagram.com/d.freeman.art/" target="_blank" rel="noopener noreferrer"><SocialIcon network="instagram" color="#fff" style={{ height: 30, width: 30 }}/></a>
+                  </div>
+                  <div className="footer-icon">
+                      <a href="https://github.com/josephjennings3" target="_blank" rel="noopener noreferrer"><SocialIcon network="github" color="#fff" style={{ height: 30, width: 30 }} /></a>
+                  </div>
+              </section>
+              <section className="signature">
+                  <div className="credit">
+                      <a href= "https://www.josephjennings3.com/" target="_blank">
+                        Developed by Joseph Jennings III
+                        </a>
+                  </div>
+              </section>
+          </div>
+      </div>
+    );
+  }
 }
 
 export default Footer;
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -12,16 +12,17 @@ class Contact extends Component {
                   Say Hello
                 </div>
                 <div className="contact-subtitle">
-                  Interested in Web Development? Are you currently hiring for a developer? Just want to say hi? You can contact me or learn more about me through any one of these means.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
                 </div>
               </section>
               <main className="contact-main">
                 <div className="contact-email">
                   <a href="mailto:jajenningsiii@email.com"><SocialIcon network="email" /></a>
                 </div>
-                <div className="-contact-linkedin">
-                  <a href="https://www.linkedin.com/in/josephjennings3/" target="_blank" rel="noopener noreferrer"><SocialIcon network="linkedin" /></a>
-                </div>
+                <div className="footer-icon">
+                      <a href="https://www.instagram.com/d.freeman.art/" target="_blank" rel="noopener noreferrer"><SocialIcon network="instagram" color="#fff" /></a>
+                  </div>
                 <div className="contact-github">
                   <a href="https://github.com/josephjennings3" target="_blank" rel="noopener noreferrer"><SocialIcon network="github" /></a>
                 </div>
